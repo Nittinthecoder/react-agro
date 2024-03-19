9af72a740d0a38a0ef914f603bd0265c3b50bc3b
Nittinthecoder <93333941+Nittinthecoder@users.noreply.github.com>
2024-03-19 14:01:24 +0530

    8

--- src/features/auth/components/Signup.js
+++ src/features/auth/components/Signup.js
@@ -16,7 +16,7 @@ export function Signup() {
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
           className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          src="https://i.pinimg.com/originals/30/34/5a/30345a99c8bc23f42d6b9848227b1f0d.jpg"
           alt="Your Company"
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-text">
