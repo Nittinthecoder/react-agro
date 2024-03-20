607283f3b355703cc7e3c85d187870398d95944d
Nittinthecoder <93333941+Nittinthecoder@users.noreply.github.com>
2024-03-20 21:39:22 +0530

    add login api

--- src/features/auth/components/Signup.js
+++ src/features/auth/components/Signup.js
@@ -1,16 +1,17 @@
+/* eslint-disable no-useless-escape */
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-// import { useSelector, useDispatch } from "react-redux";
-// import { increment, incrementAsync, selectCount } from "../authSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { createUserAsync, selectLoggedInUser } from "../authSlice";
 
 import { Link } from "react-router-dom";
 
 export function Signup() {
-  // const count = useSelector(selectCount);
-  // const dispatch = useDispatch();
+  const user = useSelector(selectLoggedInUser);
+  const dispatch = useDispatch();
   const {
     register,
     handleSubmit,
@@ -22,125 +23,136 @@ export function Signup() {
   console.log(errors);
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://i.pinimg.com/originals/30/34/5a/30345a99c8bc23f42d6b9848227b1f0d.jpg"
-          alt="Your Company"
-        />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-text">
-          Create a New Account
-        </h2>
-      </div>
+    <>
+      {user?.email}
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://i.pinimg.com/originals/30/34/5a/30345a99c8bc23f42d6b9848227b1f0d.jpg"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-text">
+            Create a New Account
+          </h2>
+        </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form
-          noValidate
-          className="space-y-6"
-          onSubmit={handleSubmit((data) => {
-            console.log(data);
-          })}
-        >
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                {...register("email", {
-                  required: "Email is required",
-                  pattern: {
-                    value: /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi,
-                    message: "email is not Validate",
-                  },
-                })}
-                type="email"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
-              />
-              {errors.email && (
-                <p className="text-red-900">{errors.email.message}</p>
-              )}
-            </div>
-          </div>
-
-          <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                {...register("password", {
-                  required: "Type the password",
-                  pattern: {
-                    value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm,
-                    message: "- at least 8 characters must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number Can contain special characters",
-                  },
-                })}
-                type="password"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
-              />
-              {errors.password && (
-                <p className="text-red-900">{errors.password.message}</p>
-              )}
-            </div>
-
-            <div className="flex items-center justify-between mt-6">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Confirm Password
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="confirmPassword"
-                {...register("confirmPassword", {
-                  required: "The password must be same",
-                  validate: (value, formValues) => value === formValues.password || "password is not matched",
-                  
-                })}
-                type="password"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
-              />
-              {errors.confirmPassword && (
-                <p className="text-red-900">{errors.confirmPassword.message}</p>
-              )}
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-accent px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:text-text focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Continue
-            </button>
-          </div>
-        </form>
-
-        <p className="mt-10 text-center text-sm text-text">
-          Already a member?{" "}
-          <Link
-            to="/login"
-            href="#"
-            className="font-semibold leading-6 text-secondary hover:text-accent"
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form
+            noValidate
+            className="space-y-6"
+            onSubmit={handleSubmit((data) => {
+              dispatch(
+                createUserAsync({ email: data.email, password: data.password })
+              );
+              console.log(data);
+            })}
           >
-            Sign in
-          </Link>
-        </p>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value: /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi,
+                      message: "email is not Validate",
+                    },
+                  })}
+                  type="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+                />
+                {errors.email && (
+                  <p className="text-red-900">{errors.email.message}</p>
+                )}
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  {...register("password", {
+                    required: "Type the password",
+                    pattern: {
+                      value:
+                        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm,
+                      message:
+                        "- at least 8 characters must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number Can contain special characters",
+                    },
+                  })}
+                  type="password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+                />
+                {errors.password && (
+                  <p className="text-red-900">{errors.password.message}</p>
+                )}
+              </div>
+
+              <div className="flex items-center justify-between mt-6">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Confirm Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="confirmPassword"
+                  {...register("confirmPassword", {
+                    required: "The password must be same",
+                    validate: (value, formValues) =>
+                      value === formValues.password ||
+                      "password is not matched",
+                  })}
+                  type="password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+                />
+                {errors.confirmPassword && (
+                  <p className="text-red-900">
+                    {errors.confirmPassword.message}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-accent px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:text-text focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Continue
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-text">
+            Already a member?{" "}
+            <Link
+              to="/login"
+              href="#"
+              className="font-semibold leading-6 text-secondary hover:text-accent"
+            >
+              Sign in
+            </Link>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
